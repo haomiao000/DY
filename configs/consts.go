8777eb013d12c263e6a017990673348be19e22a5
LZXOVO <3288820736@qq.com>
2024-07-13 17:42:25 +0800

    fix: mysql密码

--- configs/consts.go
+++ configs/consts.go
@@ -1,10 +1,11 @@
 package configs
 
-const(
-    DBUser     = "root"
-    DBPassword = "11111111"
-    DBIP       = "127.0.0.1"
-    DBPort     = "3306"
-    DBName     = "DY"
+const (
+	DBUser     = "root"
+	DBPassword = "LZXlzx251866"
+	DBIP       = "127.0.0.1"
+	DBPort     = "3306"
+	DBName     = "DY"
 )
+
 var MySecret = []byte("this is a very complex secret")
