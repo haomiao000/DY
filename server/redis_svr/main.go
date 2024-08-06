b84210ec82c762a76ad0ca15d4e5111728d484d2
LZXOVO <3288820736@qq.com>
2024-08-06 23:33:21 +0800

    feat: 通过grpc.yaml管理配置，不需要每次修改配置都重新编译

--- server/redis_svr/main.go
+++ server/redis_svr/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"net"
 
+	"github.com/haomiao000/DY/comm/discovery"
 	"github.com/haomiao000/DY/server/redis_svr/api"
+	"github.com/haomiao000/DY/server/redis_svr/config"
 	"github.com/haomiao000/DY/server/redis_svr/internal"
 	pb "github.com/haomiao000/DY/server/redis_svr/pb/redis_svr"
 	"google.golang.org/grpc"
@@ -15,8 +17,8 @@ func main() {
 		fmt.Printf("init error: %v", err)
 		return
 	}
-	lis, err := net.Listen("tcp", "localhost:50051")
 
+	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", config.GetPort()))
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -24,6 +26,7 @@ func main() {
 	s := grpc.NewServer()
 	impl := &api.RedisSvrImpl{}
 	pb.RegisterRedisSvrServer(s, impl)
+	discovery.Register(config.GetServiceName(), config.GetAddress())
 	if err = s.Serve(lis); err != nil {
 		fmt.Println(err)
 	}
